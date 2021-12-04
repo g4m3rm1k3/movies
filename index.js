2939d2f85321b3b675ce6f0d4cbfd137ed97ef16
Michael McLean <g4m3rm1k3@hotmail.com>
2021-12-04 18:32:59 -0500

    fixed string to int bug

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ const runComparison = () => {
     console.log(rightStat.dataset.value);
     console.log(leftStat.dataset.value);
 
-    const leftSideValue = leftStat.dataset.value;
-    const rightSideValue = rightStat.dataset.value;
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
 
     if (rightSideValue > leftSideValue) {
       leftStat.classList.remove("is-primary");
